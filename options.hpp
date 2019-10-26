035e643b75010036cdfe6669394aa643d2990fc3
Bethoth <45798269+Bethoth@users.noreply.github.com>
2019-10-26 21:16:35 +0200

    Addition of units system (#4)
    * Make pi a const value
    
    * Addition of units
    
    * Addition of units and units options

--- options.hpp
+++ options.hpp
@@ -9,8 +9,20 @@ namespace options {
 
 	double rounder = 1000.0;
 
-	std::array<std::string, 2> options = { "Sleep time", "Rounder" };
+	std::string length_unit = "cm";
+	
+	std::string area_unit = "cm^2";
 
-	int options_size = std::tuple_size<std::array<std::string, 2>>::value;
+	std::string volume_unit = "cm^3";
+
+	std::string voluminal_mass_unit = "kg/cm^3";
+
+	std::array<std::string, 4> units = { "Length", "Area", "Volume", "Voluminal mass" };
+
+	int units_size = std::tuple_size<std::array<std::string, 4>>::value;
+
+	std::array<std::string, 3> options = { "Sleep time", "Rounder", "Units" };
+
+	int options_size = std::tuple_size<std::array<std::string, 3>>::value;
 
 }
